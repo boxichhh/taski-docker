4acdd5c67ce538ebe1fc78bfd9b2520907190055
boxichhh <orobij.list@gmail.com>
2025-08-20 19:30:24 +0000

    users

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,10 +8,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.187.61', '127.0.0.1', 'taski-dockerhub.duckdns.org']
+ALLOWED_HOSTS = ['localhost', '89.169.187.61', '127.0.0.1', 'taski-dockerhub.hopto.org']
 
 
-# Application definition
+# Application definiion
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
